42d24db0e3b63ded6b9fa57bb1f6eb91a52e11df
richard98 <richard.suarezjacome@gmail.com>
2022-01-14 16:29:42 -0500

    Agregar archivos de proyecto.

---
+++ WfAdoNetPersona/Form1.Designer.cs
@@ -0,0 +1,325 @@
+﻿
+namespace WfAdoNetPersona
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Label1 = new System.Windows.Forms.Label();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.btnInsertar = new System.Windows.Forms.Button();
+            this.txtNacimiento = new System.Windows.Forms.DateTimePicker();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtApellido = new System.Windows.Forms.TextBox();
+            this.txtEdad = new System.Windows.Forms.TextBox();
+            this.txtCedula = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.cmbPais = new System.Windows.Forms.ComboBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.txtBuscarNombre = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Label1
+            // 
+            this.Label1.AutoSize = true;
+            this.Label1.Location = new System.Drawing.Point(19, 25);
+            this.Label1.Name = "Label1";
+            this.Label1.Size = new System.Drawing.Size(16, 13);
+            this.Label1.TabIndex = 0;
+            this.Label1.Text = "Id";
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(79, 25);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(48, 20);
+            this.txtId.TabIndex = 1;
+            // 
+            // btnInsertar
+            // 
+            this.btnInsertar.Location = new System.Drawing.Point(22, 282);
+            this.btnInsertar.Name = "btnInsertar";
+            this.btnInsertar.Size = new System.Drawing.Size(285, 23);
+            this.btnInsertar.TabIndex = 2;
+            this.btnInsertar.Text = "Insertar";
+            this.btnInsertar.UseVisualStyleBackColor = true;
+            this.btnInsertar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtNacimiento
+            // 
+            this.txtNacimiento.Location = new System.Drawing.Point(79, 172);
+            this.txtNacimiento.Name = "txtNacimiento";
+            this.txtNacimiento.Size = new System.Drawing.Size(210, 20);
+            this.txtNacimiento.TabIndex = 3;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.cmbPais);
+            this.groupBox1.Controls.Add(this.btnInsertar);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.txtCedula);
+            this.groupBox1.Controls.Add(this.txtEdad);
+            this.groupBox1.Controls.Add(this.txtApellido);
+            this.groupBox1.Controls.Add(this.txtNombre);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.txtNacimiento);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.Label1);
+            this.groupBox1.Controls.Add(this.txtId);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(323, 322);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Resgitro de Personas";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(19, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Nombre";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(19, 95);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Apellido";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(19, 133);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(32, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Edad";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(19, 172);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(60, 13);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Nacimiento";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(79, 57);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(210, 20);
+            this.txtNombre.TabIndex = 6;
+            // 
+            // txtApellido
+            // 
+            this.txtApellido.Location = new System.Drawing.Point(79, 95);
+            this.txtApellido.Name = "txtApellido";
+            this.txtApellido.Size = new System.Drawing.Size(210, 20);
+            this.txtApellido.TabIndex = 7;
+            // 
+            // txtEdad
+            // 
+            this.txtEdad.Location = new System.Drawing.Point(79, 133);
+            this.txtEdad.Name = "txtEdad";
+            this.txtEdad.Size = new System.Drawing.Size(210, 20);
+            this.txtEdad.TabIndex = 8;
+            // 
+            // txtCedula
+            // 
+            this.txtCedula.Location = new System.Drawing.Point(79, 209);
+            this.txtCedula.Name = "txtCedula";
+            this.txtCedula.Size = new System.Drawing.Size(210, 20);
+            this.txtCedula.TabIndex = 9;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(19, 212);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(40, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Cedula";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(19, 244);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(27, 13);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "Pais";
+            // 
+            // cmbPais
+            // 
+            this.cmbPais.FormattingEnabled = true;
+            this.cmbPais.Location = new System.Drawing.Point(79, 244);
+            this.cmbPais.Name = "cmbPais";
+            this.cmbPais.Size = new System.Drawing.Size(210, 21);
+            this.cmbPais.TabIndex = 12;
+            this.cmbPais.Text = "<Seleccione Pais>";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(355, 59);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(401, 275);
+            this.dataGridView1.TabIndex = 5;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            this.dataGridView1.SelectionChanged += new System.EventHandler(this.dataGridView1_SelectionChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 347);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "Nuevo";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(260, 347);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 14;
+            this.button3.Text = "Salir";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(579, 347);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(177, 23);
+            this.button4.TabIndex = 15;
+            this.button4.Text = "Eliminar";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(355, 347);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(177, 23);
+            this.button5.TabIndex = 16;
+            this.button5.Text = "Buscar";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // txtBuscarNombre
+            // 
+            this.txtBuscarNombre.Location = new System.Drawing.Point(447, 21);
+            this.txtBuscarNombre.Name = "txtBuscarNombre";
+            this.txtBuscarNombre.Size = new System.Drawing.Size(210, 20);
+            this.txtBuscarNombre.TabIndex = 13;
+            this.txtBuscarNombre.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(387, 24);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(44, 13);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "Nombre";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(780, 382);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.txtBuscarNombre);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Label1;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.Button btnInsertar;
+        private System.Windows.Forms.DateTimePicker txtNacimiento;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtEdad;
+        private System.Windows.Forms.TextBox txtApellido;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtCedula;
+        private System.Windows.Forms.ComboBox cmbPais;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox txtBuscarNombre;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ WfAdoNetPersona/Form1.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WfAdoNetPersona
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.cargarDatos();
+            this.CargarComboBox();
+            txtId.Enabled = false;
+            btnInsertar.Text = "Insertar";
+
+        }
+
+        public void CargarComboBox()
+        {
+            using (DatosPersonalesEntities conn = new DatosPersonalesEntities())
+            {
+                dynamic lista = conn.pais.ToList();
+                ///cmbPais.Text = "<Seleccione su País>";
+                cmbPais.DataSource = lista;
+                cmbPais.DisplayMember = "nombre";
+                cmbPais.ValueMember = "id";
+            }
+            // comboBox1.Items.Add(lista);
+
+        }
+
+        public void cargarDatos()
+        {
+            using (DatosPersonalesEntities conn = new DatosPersonalesEntities())
+            {
+                dataGridView1.DataSource = conn.Personas.Select(x => new {
+                    x.Id,
+                    x.Nombre,
+                    x.Apellido,
+                    x.Edad,
+                    x.FechaNacimiento,
+                    x.Cedula,
+                    Pais = x.pais1.nombre
+                }).ToList();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (DatosPersonalesEntities conn = new DatosPersonalesEntities())
+            {
+                try
+                {
+                    Personas per = new Personas();
+                    per.Nombre = txtNombre.Text;
+                    per.Apellido = txtApellido.Text;
+                    per.Edad = Convert.ToInt32(txtEdad.Text);
+                    per.FechaNacimiento = Convert.ToDateTime(txtNacimiento.Text);
+                    per.Cedula = txtCedula.Text;
+                    per.pais = (int?)cmbPais.SelectedValue;
+
+                    if (dataGridView1.SelectedRows.Count==1)
+                    {
+                        int id = Convert.ToInt32(dataGridView1.CurrentRow.Cells[0].Value);
+                        per.Id = id;
+                        conn.Entry(per).State=EntityState.Modified;
+                        conn.SaveChanges();
+                        MessageBox.Show("Exito al Modificar");
+                        LimpiarPantalla();
+                        cargarDatos();
+                    }
+                    else
+                    {
+                        conn.Personas.Add(per);
+                        conn.SaveChanges();
+                        MessageBox.Show("Exito al Guardar");
+                        LimpiarPantalla();
+                        cargarDatos();
+                    }
+                    
+                   
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+
+
+            }
+        }
+
+        public void LimpiarPantalla()
+        {
+            txtId.Clear();
+            txtNombre.Clear();
+            txtApellido.Clear();
+            txtEdad.Clear();
+            txtCedula.Clear();
+            btnInsertar.Text = "Insertar";
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+           
+        }
+
+        private void dataGridView1_SelectionChanged(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count == 1)
+            {
+                txtId.Text = Convert.ToString(dataGridView1.CurrentRow.Cells[0].Value);
+                txtNombre.Text = Convert.ToString(dataGridView1.CurrentRow.Cells[1].Value);
+                txtApellido.Text = Convert.ToString(dataGridView1.CurrentRow.Cells[2].Value);
+                txtEdad.Text = Convert.ToString(dataGridView1.CurrentRow.Cells[3].Value);
+                txtNacimiento.Text = Convert.ToString(dataGridView1.CurrentRow.Cells[4].Value);
+                txtCedula.Text = Convert.ToString(dataGridView1.CurrentRow.Cells[5].Value);
+                cmbPais.Text = Convert.ToString(dataGridView1.CurrentRow.Cells[6].Value);
+                btnInsertar.Text = "Modificar";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            LimpiarPantalla();
+            btnInsertar.Text = "Insertar";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            using (DatosPersonalesEntities conn = new DatosPersonalesEntities())
+            {
+                if (dataGridView1.SelectedRows.Count==1)
+                {
+                    int id = Convert.ToInt32(dataGridView1.CurrentRow.Cells[0].Value);
+                    var h = conn.Personas.Where(x => x.Id == id).FirstOrDefault();
+                    conn.Personas.Remove(h);
+                    conn.SaveChanges();
+                    MessageBox.Show("Exito al Eliminar");
+                    LimpiarPantalla();
+                    cargarDatos();
+                }
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            using (DatosPersonalesEntities conn = new DatosPersonalesEntities())
+            {
+                
+                    dynamic lis = conn.Personas.Select(x=> new {
+                        x.Id,
+                        x.Nombre,
+                        x.Apellido,
+                        x.Edad,
+                        x.FechaNacimiento,
+                        x.Cedula,
+                        Pais = x.pais1.nombre
+                    }).Where(x=> x.Nombre==txtBuscarNombre.Text).ToList();
+                    dataGridView1.DataSource = lis; 
+                
+            }
+
+        }
+
+
+
+    }
+}
